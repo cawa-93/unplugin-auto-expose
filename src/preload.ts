f3740e17e4e8c8a98c78da9564dc2e887d1cd915
Tanimodori <unknowner2014@gmail.com>
2022-07-25 19:01:29 +0800

    fix: Fix createUnplugin signature

--- src/preload.ts
+++ src/preload.ts
@@ -1,20 +1,22 @@
 import { createUnplugin } from 'unplugin';
 import { PreloadOptions } from './types';
 
-export const preload = createUnplugin((options: PreloadOptions, meta) => {
-  return {
-    // common unplugin hooks
-    name: 'unplugin-auto-expose-preload',
+export const preload = createUnplugin(
+  (_options: PreloadOptions | undefined) => {
+    return {
+      // common unplugin hooks
+      name: 'unplugin-auto-expose-preload',
 
-    renderChunk(code, info) {
-      if (!info.isEntry) {
-        return;
-      }
-      code += "\nconst {contextBridge} = require('electron');\n";
-      for (const exp of info.exports) {
-        code += `;\ncontextBridge.exposeInMainWorld('__electron_preload__${exp}', exports.${exp});\n`;
-      }
-      return code;
-    },
-  };
-});
+      renderChunk(code: string, info: { isEntry: boolean; exports: string[] }) {
+        if (!info.isEntry) {
+          return;
+        }
+        code += "\nconst {contextBridge} = require('electron');\n";
+        for (const exp of info.exports) {
+          code += `;\ncontextBridge.exposeInMainWorld('__electron_preload__${exp}', exports.${exp});\n`;
+        }
+        return code;
+      },
+    };
+  },
+);
